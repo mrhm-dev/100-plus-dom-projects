bc2bcfeced2aff4cc2e6a87d268dc0f53661ba93
HM Nayem <hasan.m.nayem@gmail.com>
2022-04-08 20:09:44 +0600

    Revert "Merge branch 'master' of https://github.com/mrhm-dev/100-plus-dom-projects"
    This reverts commit 8b3c11e9b9725fadca3f35d50cbd61cc781f79f9, reversing
    changes made to 71d06a0882223a0b396de858890660eb5dc27898.

--- examples/Faq/dist/scripts/functions.js
+++
@@ -1,6 +0,0 @@
-export function removeClassFromNodelists(nodeLists, className) {
-    nodeLists.forEach(element => {
-        element.parentNode.classList.remove(className);
-        element.querySelector('.faq-icon').src = './assets/plus-icon.svg';
-    });
-}
